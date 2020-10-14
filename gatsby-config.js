df8ed26f9006a5e13cbd20c1f914651f853d89bb
Frank Showalter <fshowalter@gmail.com>
2020-10-13 20:14:17 -0400

    add feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,7 +87,75 @@ module.exports = {
         legacy: false,
         theme_color_in_head: false,
       },
-    }, // this (optional) plugin enables Progressive Web App + Offline functionality
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map((edge) => {
+                return {
+                  ...edge.node.frontmatter,
+                  date: edge.node.frontmatter.date,
+                  url: `${site.siteMetadata.siteUrl}/reviews/${edge.node.frontmatter.slug}/`,
+                  guid: `${site.siteMetadata.siteUrl}/${edge.node.frontmatter.sequence}-${edge.node.frontmatter.slug}`,
+                  custom_elements: [
+                    {
+                      "content:encoded": `<image src="${edge.node.image.childImageSharp.resize.src}" alt="A still from ${edge.node.frontmatter.title}">${edge.node.linkedExcerpt}`,
+                    },
+                  ],
+                };
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___sequence] },
+                  limit: 25,
+                  filter: {fileAbsolutePath: {regex: "content/reviews/.*.md$/"}}
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      linkedExcerpt
+                      frontmatter {
+                        title
+                        date
+                        slug
+                        sequence
+                      }
+                      image: backdrop {
+                        childImageSharp {
+                          resize(toFormat: JPG, width: 800, quality: 80) {
+                            src
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/feed.xml",
+            title: "Frank's Movie Log",
+          },
+        ],
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     "gatsby-plugin-preact",
