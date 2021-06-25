879cfe14447deb2f03497ab5d3dd5060f1146f81
Frank Showalter <fshowalter@gmail.com>
2021-06-24 23:59:05 -0400

    add reviews / icons

---
+++ content/reviews/1115-funny-games-1997.md
@@ -0,0 +1,11 @@
+---
+sequence: 1115
+date: 2021-06-24
+imdb_id: tt0119167
+title: Funny Games (1997)
+grade: A
+slug: funny-games-1997
+venue: Blu-ray
+venue_notes:
+---
+
--- src/components/WatchlistIndexPage/WatchlistIndexPage.tsx
+++ src/components/WatchlistIndexPage/WatchlistIndexPage.tsx
@@ -426,7 +426,49 @@ export default function WatchlistIndexPage({
               <Button
                 onClick={() => dispatch({ type: ActionType.TOGGLE_REVIEWED })}
               >
-                {state.hideReviewed ? "Show Reviewed" : "Hide Reviewed"}
+                {state.hideReviewed ? (
+                  <>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                      />
+                    </svg>{" "}
+                    Show Reviewed
+                  </>
+                ) : (
+                  <>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+                      />
+                    </svg>{" "}
+                    Hide Reviewed
+                  </>
+                )}
               </Button>
             )}
           </div>
@@ -452,6 +494,18 @@ export default function WatchlistIndexPage({
           </ol>
           <div className={showMoreCss}>
             <Button onClick={() => dispatch({ type: ActionType.SHOW_MORE })}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                  clipRule="evenodd"
+                />
+              </svg>
               Show More
             </Button>
           </div>
