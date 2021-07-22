5e2ef0bfbf68e7ae1983c1a3b087ab38704eba71
Frank Showalter <fshowalter@gmail.com>
2021-07-22 16:51:16 -0400

    fix buttons

--- src/components/WatchlistIndexPage/WatchlistIndexPage.tsx
+++ src/components/WatchlistIndexPage/WatchlistIndexPage.tsx
@@ -122,7 +122,7 @@ function WatchlistMovieCheckMark({
         <path
           strokeLinecap="round"
           strokeLinejoin="round"
-          strokeWidth={1}
+          strokeWidth={0.5}
           d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
         />
       </svg>
@@ -425,49 +425,7 @@ export default function WatchlistIndexPage({
               <Button
                 onClick={() => dispatch({ type: ActionType.TOGGLE_REVIEWED })}
               >
-                {state.hideReviewed ? (
-                  <>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-6 w-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                      />
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                      />
-                    </svg>{" "}
-                    Show Reviewed
-                  </>
-                ) : (
-                  <>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-6 w-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
-                      />
-                    </svg>{" "}
-                    Hide Reviewed
-                  </>
-                )}
+                {state.hideReviewed ? "Show Reviewed" : "Hide Reviewed"}
               </Button>
             )}
           </div>
@@ -495,16 +453,11 @@ export default function WatchlistIndexPage({
             {state.filteredMovies.length > state.showCount && (
               <Button onClick={() => dispatch({ type: ActionType.SHOW_MORE })}>
                 <svg
+                  focusable="false"
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
+                  viewBox="0 0 24 24"
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M15.707 4.293a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 011.414-1.414L10 8.586l4.293-4.293a1 1 0 011.414 0zm0 6a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 111.414-1.414L10 14.586l4.293-4.293a1 1 0 011.414 0z"
-                    clipRule="evenodd"
-                  />
+                  <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
                 </svg>
                 Show More
               </Button>
