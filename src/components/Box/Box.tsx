f09a08692bd191567e91b33f6cb252a544f2f39f
Frank Showalter <fshowalter@gmail.com>
2023-07-11 07:37:49 -0400

    update types

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -49,9 +49,7 @@ type HTMLProperties = Omit<
 >;
 
 type OverrideTokens<T> = {
-  [K in keyof T as K extends string ? `__${K}` : number]:
-    | any
-    | Record<string, unknown>;
+  [K in keyof T as K extends string ? `__${K}` : number]: any;
 };
 
 interface CreateBoxParams<AtomsFn> {
