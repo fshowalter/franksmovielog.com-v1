b8f65efe48a0f2c0d4e8da68a1aeed63b5e94b76
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2024-04-15 16:50:29 -0400

    add specs

---
+++ src/pages/cast-and-crew.spec.tsx
@@ -0,0 +1,104 @@
+import { act, render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import CastAndCrewPage, { Head } from "./cast-and-crew";
+import { data } from "./cast-and-crew.fixtures";
+
+describe("/watchlist/performers/", () => {
+  it("can filter by name", async () => {
+    expect.hasAssertions();
+
+    render(<CastAndCrewPage data={data} />);
+
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    await act(async () => {
+      await userEvent.type(screen.getByLabelText("Name"), "John Wayne");
+      await new Promise((r) => setTimeout(r, 500));
+    });
+
+    expect(screen.getByTestId("entity-list")).toMatchSnapshot();
+  });
+
+  it("can sort by name desc", async () => {
+    expect.hasAssertions();
+
+    render(<CastAndCrewPage data={data} />);
+
+    await userEvent.selectOptions(
+      screen.getByLabelText("Order By"),
+      "Name (Z → A)",
+    );
+
+    expect(screen.getByTestId("entity-list")).toMatchSnapshot();
+  });
+
+  it("can sort by name asc", async () => {
+    expect.hasAssertions();
+
+    render(<CastAndCrewPage data={data} />);
+
+    await userEvent.selectOptions(
+      screen.getByLabelText("Order By"),
+      "Name (A → Z)",
+    );
+
+    expect(screen.getByTestId("entity-list")).toMatchSnapshot();
+  });
+
+  it("can sort by title count desc", async () => {
+    expect.hasAssertions();
+
+    render(<CastAndCrewPage data={data} />);
+
+    await userEvent.selectOptions(
+      screen.getByLabelText("Order By"),
+      "Title Count (Most First)",
+    );
+
+    expect(screen.getByTestId("entity-list")).toMatchSnapshot();
+  });
+
+  it("can sort by title count asc", async () => {
+    expect.hasAssertions();
+
+    render(<CastAndCrewPage data={data} />);
+
+    await userEvent.selectOptions(
+      screen.getByLabelText("Order By"),
+      "Title Count (Fewest First)",
+    );
+
+    expect(screen.getByTestId("entity-list")).toMatchSnapshot();
+  });
+
+  it("can sort by review count desc", async () => {
+    expect.hasAssertions();
+
+    render(<CastAndCrewPage data={data} />);
+
+    await userEvent.selectOptions(
+      screen.getByLabelText("Order By"),
+      "Review Count (Most First)",
+    );
+
+    expect(screen.getByTestId("entity-list")).toMatchSnapshot();
+  });
+
+  it("can sort by review count asc", async () => {
+    expect.hasAssertions();
+
+    render(<CastAndCrewPage data={data} />);
+
+    await userEvent.selectOptions(
+      screen.getByLabelText("Order By"),
+      "Review Count (Fewest First)",
+    );
+
+    expect(screen.getByTestId("entity-list")).toMatchSnapshot();
+  });
+
+  it("sets page title", () => {
+    render(<Head />);
+
+    expect(document.title).toStrictEqual("Cast and Crew");
+  });
+});
